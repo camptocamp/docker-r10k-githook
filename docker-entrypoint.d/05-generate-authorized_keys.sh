3f12d39a498ba99c86d6bb2785dba57e8b5fa43b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-10 15:58:44 +0100

    Fix private key generation

--- docker-entrypoint.d/05-generate-authorized_keys.sh
+++ docker-entrypoint.d/05-generate-authorized_keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test -n "${RSA_PRIVATE_KEY}"; then
-  echo $RSA_PRIVATE_KEY > ~r10k/.ssh/id_rsa
+  echo -e $RSA_PRIVATE_KEY > ~r10k/.ssh/id_rsa
   chmod 0600 ~r10k/.ssh/id_rsa
   ssh-keygen -y -f ~r10k/.ssh/id_rsa > ~r10k/.ssh/id_rsa.pub
 fi
