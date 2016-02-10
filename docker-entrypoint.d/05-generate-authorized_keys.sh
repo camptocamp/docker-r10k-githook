71e2fe05ba2d08be9c93a6f7d5898c51a9d5819b
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-02-10 15:31:45 +0100

    Fix key permission

--- docker-entrypoint.d/05-generate-authorized_keys.sh
+++ docker-entrypoint.d/05-generate-authorized_keys.sh
@@ -2,5 +2,6 @@
 
 if test -n "${RSA_PRIVATE_KEY}"; then
   echo $RSA_PRIVATE_KEY > ~r10k/.ssh/id_rsa
+  chmod 0600 ~r10k/.ssh/id_rsa
   ssh-keygen -y -f ~r10k/.ssh/id_rsa > ~r10k/.ssh/id_rsa.pub
 fi
